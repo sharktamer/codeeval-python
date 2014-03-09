4262b2491e0826508176cbf46967d1e99eddfb0b
sharktamer <priestoftime@gmail.com>
2014-03-09 19:36:53 +0000

    Added solution for #39

---
+++ 039-HappyNumbers.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+
+
+def isHappy(n):
+    while True:
+        if n == '1':
+            return 1
+        elif n == '4':
+            return 0
+        n = str(sum([int(i)**2 for i in n]))
+
+with open(sys.argv[1]) as f:
+    for line in f:
+        print isHappy(line.strip())
