ad16d35247e32eb840d18140fbb7020cfff6418a
sharktamer <priestoftime@gmail.com>
2014-03-09 01:07:09 +0000

    Added alternate solution for #131

--- 131-Easy-SplitTheNumber.py
+++ 131-Easy-SplitTheNumber.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python
 
 import sys
-import re
+# import re
+
+# with open(sys.argv[1]) as f:
+#     for line in f:
+#         n, e = line.split()
+#         p = re.search('[\+\-]', e).start()
+#         print eval(n[:p] + e[p] + n[p:])
+
+from string import maketrans
 
 with open(sys.argv[1]) as f:
     for line in f:
         n, e = line.split()
-        p = re.search('[\+\-]', e).start()
-        print eval(n[:p] + e[p] + n[p:])
+        ec = e.replace('+', '').replace('-', '')
+        t = maketrans(ec, n)
+        print eval(e.translate(t))
