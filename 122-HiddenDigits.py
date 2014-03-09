e92fef0fec234d71f7851c2bfe989d3de187e1c8
sharktamer <priestoftime@gmail.com>
2014-03-09 19:20:01 +0000

    Added solution for #122

---
+++ 122-HiddenDigits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+import sys
+import re
+import string
+
+m = string.maketrans('abcdefghij', '0123456789')
+
+with open(sys.argv[1]) as f:
+    for line in f:
+        trans = line.strip().translate(m)
+        out = "".join(re.findall(r'\d', trans))
+        if len(out) == 0:
+            print 'NONE'
+        else:
+            print out
