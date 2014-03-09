dc6d84e88336b627111f3a01f40fc22ef5b6b91a
sharktamer <priestoftime@gmail.com>
2014-03-09 15:08:52 +0000

    Replaced two tuples with tuple of tuples

--- 106-RomanNumerals.py
+++ 106-RomanNumerals.py
@@ -4,12 +4,12 @@ import sys
 
 
 def romanMe(n, s=''):
-    digits = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
-    numerals = ('M', 'CM', 'D', 'CD', 'C',
-                'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
-    for digit, numeral in zip(digits, numerals):
+    numerals = ((1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'),
+               (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'),
+               (5, 'V'), (4, 'IV'), (1, 'I'))
+    for digit, numeral in numerals:
         if n >= digit:
-            return romanMe(n-digit, s+numeral)
+            return romanMe(n - digit, s + numeral)
     return s
 
 with open(sys.argv[1]) as f:
