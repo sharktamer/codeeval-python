758236b44fb41985d958b055f28857fa9722af65
sharktamer <donohoe.shane@gmail.com>
2015-02-20 22:46:36 +0000

    Add solution for 183-Details

---
+++ 183-Easy-Details.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+import re
+
+in_file = sys.argv[1]
+
+with open(in_file) as f:
+    for line in f:
+        print min([len(re.search(r'X(\.*)Y', i).group(1)) for i in line.split(',')])
+
