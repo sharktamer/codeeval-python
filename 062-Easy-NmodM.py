87a99dffbd08442827dbae13ec5d4addee809aa0
sharktamer <donohoe.shane@gmail.com>
2015-03-11 11:56:00 +0000

    Added solution for #62: Easy N mod M

---
+++ 062-Easy-NmodM.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+
+import sys
+from itertools import count
+
+def myMod(x, y):
+    for i in count(0, y):
+        if i > a:
+            return a - (i - b)
+
+with open(sys.argv[1]) as f:
+    for line in f:
+        a, b = map(int, line.split(','))
+        print(myMod(a, b))
+
