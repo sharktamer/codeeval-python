1936dda5519b573a516a3d3f10fc56e534ca5307
sharktamer <donohoe.shane@gmail.com>
2015-03-14 23:01:02 +0000

    Added solution for #102: Json Menu IDs

---
+++ 102-JsonMenuIDs.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import sys
+import re
+
+r = re.compile(r'"Label (.*?)"')
+
+with open(sys.argv[1]) as f:
+    lines = f.read().strip().splitlines()
+lines = [i for i in lines if i]
+
+for line in lines:
+    out = sum(int(i) for i in r.findall(line) if line.strip())
+    print(out)
