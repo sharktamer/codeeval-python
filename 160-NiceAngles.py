54a8b5f7d27987926390cdfc08d7c3147be84b39
sharktamer <donohoe.shane@gmail.com>
2015-03-15 23:43:26 +0000

    Add solution for #160: Nice angles

---
+++ 160-NiceAngles.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+
+import sys
+
+with open(sys.argv[1]) as f:
+    lines = f.read().strip().splitlines()
+
+for line in lines:
+    num = float(line)
+    degrees = int(num)
+    minutes = int(num % 1 * 60)
+    seconds = int(num % 1 * 60 % 1 * 60)
+    out = '{0}.{1:02d}\'{2:02}"'.format(degrees, minutes, seconds)
+    print(out)
