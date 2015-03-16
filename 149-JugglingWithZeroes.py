692311bf59ef6cded50d50714c481c77587735b4
sharktamer <donohoe.shane@gmail.com>
2015-03-16 15:18:03 +0000

    Use islice instead of list slicing

--- 149-JugglingWithZeroes.py
+++ 149-JugglingWithZeroes.py
@@ -1,12 +1,14 @@
 #!/bin/env python
 
 import sys
+from itertools import islice
 
 with open(sys.argv[1]) as f:
     lines = f.read().strip().splitlines()
 
 for line in lines:
-    pairs = zip(line.split()[::2], line.split()[1::2])
+    zeroes = line.split()
+    pairs = zip(islice(zeroes, 0, None, 2), islice(zeroes, 1, None, 2))
     binary = ''.join(seq if flag == '0' else '1'*len(seq)
                      for flag, seq in pairs)
     print(int(binary, 2))
