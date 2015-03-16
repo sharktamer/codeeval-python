23b798cb42ac521c843615060e55d4e5856a268b
sharktamer <donohoe.shane@gmail.com>
2015-03-16 15:05:45 +0000

    Add solution for #124: Road trip

---
+++ 124-RoadTrip.py
@@ -0,0 +1,13 @@
+#!/bin/env python
+
+import sys
+from itertools import islice
+
+with open(sys.argv[1]) as f:
+    lines = f.read().strip().splitlines()
+
+for line in lines:
+    distances = [0] + [int(i.split(',')[1]) for i in line.split(';') if i]
+    distances.sort()
+    out = [d2 - d1 for d1, d2 in zip(distances, islice(distances, 1, None))]
+    print(','.join(str(i) for i in out))
