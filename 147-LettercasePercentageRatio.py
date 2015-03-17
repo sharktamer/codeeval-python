9d504642304b0ad843e98c076e949a7acdab4ee2
sharktamer <donohoe.shane@gmail.com>
2015-03-17 15:12:46 +0000

    Add solution for #147: Lettercase percentage ratio

---
+++ 147-LettercasePercentageRatio.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+
+import sys
+
+with open(sys.argv[1]) as f:
+    lines = f.read().strip().splitlines()
+
+for line in lines:
+    lower = len([i for i in line if i.islower()]) / len(line) * 100
+    upper = len([i for i in line if i.isupper()]) / len(line) * 100
+    out = 'lowercase: {0:.2f} uppercase: {1:.2f}'.format(lower, upper)
+    print(out)
