c2c2a54fb1efe9831073dc86abb35816d58815ee
sharktamer <donohoe.shane@gmail.com>
2015-03-17 15:12:18 +0000

    Add solution for #156: RollerCoaster

---
+++ 156-RollerCoaster.py
@@ -0,0 +1,12 @@
+#!/bin/env python
+
+import sys
+from itertools import cycle
+
+with open(sys.argv[1]) as f:
+    lines = f.read().strip().splitlines()
+
+for line in lines:
+    c = cycle([str.upper, str.lower])
+    out = ''.join(next(c)(i) if i.isalpha() else i for i in line)
+    print(out)
