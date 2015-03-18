0a5538de91b29b9f6ed13365787ccfcbbc85fb45
sharktamer <donohoe.shane@gmail.com>
2015-03-18 00:52:32 +0000

    Swap 008.py and 008.txt contents

--- 008-Easy-ReverseWords.py
+++ 008-Easy-ReverseWords.py
@@ -1,2 +1,9 @@
-Hello World
-Hello CodeEval
+#!/usr/bin/env python
+import sys
+
+in_file = sys.argv[1]
+with open(in_file) as f:
+    file = f.readlines()
+
+for l in file:
+    print(" ".join(l.split()[::-1]))
