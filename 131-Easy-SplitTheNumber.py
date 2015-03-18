2107a9256ea4d1c398fdcd30279f9d3e6f64310a
sharktamer <donohoe.shane@gmail.com>
2015-03-18 01:07:13 +0000

    string.maketrans is str.maketrans

--- 131-Easy-SplitTheNumber.py
+++ 131-Easy-SplitTheNumber.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python
 
 import sys
-# import re
-
-# with open(sys.argv[1]) as f:
-#     for line in f:
-#         n, e = line.split()
-#         p = re.search('[\+\-]', e).start()
-#         print eval(n[:p] + e[p] + n[p:])
-
-from string import maketrans
 
 with open(sys.argv[1]) as f:
     for line in f:
         n, e = line.split()
         ec = e.replace('+', '').replace('-', '')
-        t = maketrans(ec, n)
+        t = str.maketrans(ec, n)
         print(eval(e.translate(t)))
