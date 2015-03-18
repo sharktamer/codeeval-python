4522289e5ea74fd1fbc0428cb486bd99a0de37b9
sharktamer <donohoe.shane@gmail.com>
2015-03-18 01:00:43 +0000

    Fixed len line, replaced filter with list comprehension

--- 040-Easy-SelfDescribingNumbers.py
+++ 040-Easy-SelfDescribingNumbers.py
@@ -5,7 +5,7 @@ import sys
 
 def selfDescribing(number):
     for n, d in enumerate(number):
-        if int(d) != len(filter(lambda x: x == str(n), number)):
+        if int(d) != len([i for i in number if i == str(n)]):
             return 0
     return 1
 
